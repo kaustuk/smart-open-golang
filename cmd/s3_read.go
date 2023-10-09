fcbfd3b7e56b10a10130e8e9b2c40637936d3311
kaustuk <kaustukrathod@gmail.com>
2023-10-09 18:34:20 +0530

    added localstack and s3 read dummy code

---
+++ cmd/s3_read.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func main() {
+	fmt.Printf("hello from main\n")
+
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return
+	}
+
+	s3Client := s3.NewFromConfig(sdkConfig, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String("http://localhost:4566")
+	})
+
+	result, err := s3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+		return
+	}
+	if len(result.Buckets) == 0 {
+		fmt.Println("You don't have any buckets!")
+	} else {
+
+		for _, bucket := range result.Buckets {
+			fmt.Printf("\t%v\n", *bucket.Name)
+		}
+	}
+
+}
